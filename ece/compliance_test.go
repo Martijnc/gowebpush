006b6bcada89721e0a022d1421ba8b39e1fd4709
Martijn Croonen <martijn@martijnc.be>
2016-02-25 18:33:50 +0100

    Add an extra test.

--- ece/compliance_test.go
+++ ece/compliance_test.go
@@ -18,10 +18,26 @@ func TestSpecification(t *testing.T) {
 		senderPublic   string
 		senderPrivate  string
 		salt           string
+		auth           string
 		plainText      string
 		cipherText     string
 	}{
-		{"BCEkBjzL8Z3C+oi2Q7oE5t2Np+p7osjGLg93qUP0wvqRT21EEWyf0cQDQcakQMqz4hQKYOQ3il2nNZct4HgAUQU=", "BDgpRKok2GZZDmS4r63vbJSUtcQx4Fq1V58+6+3NbZzSTlZsQiCEDTQy3CZ0ZMsqeqsEb7qW2blQHA4S48fynTk=", "vG7TmzUX9NfVR4XUGBkLAFu8iDyQe+q/165JkkN0Vlw=", "Qg61ZJRva/XBE9IEUelU3A==", "I am the walrus", "yqD2bapcx14XxUbtwjiGx69eHE3Yd6AqXcwBpT2Kd1uy"},
+		{
+			"BCEkBjzL8Z3C+oi2Q7oE5t2Np+p7osjGLg93qUP0wvqRT21EEWyf0cQDQcakQMqz4hQKYOQ3il2nNZct4HgAUQU=",
+			"BDgpRKok2GZZDmS4r63vbJSUtcQx4Fq1V58+6+3NbZzSTlZsQiCEDTQy3CZ0ZMsqeqsEb7qW2blQHA4S48fynTk=",
+			"vG7TmzUX9NfVR4XUGBkLAFu8iDyQe+q/165JkkN0Vlw=",
+			"Qg61ZJRva/XBE9IEUelU3A==",
+			"",
+			"I am the walrus",
+			"yqD2bapcx14XxUbtwjiGx69eHE3Yd6AqXcwBpT2Kd1uy"},
+		{
+			"BOLcHOg4ajSHR6BjbSBeX/6aXjMu1V5RrUYXqyV/FqtQSd8RzdU1gkMv1DlRPDIUtFK6Nd16Jql0eSzyZh4V2uc=",
+			"BG3OGHrl3YJ5PHpl0GSqtAAlUPnx1LvwQvFMIc68vhJU6nIkRzPEqtCduQz8wQj0r71NVPzr7ZRk2f+fhsQ5pK8=",
+			"Dt1CLgQlkiaA+tmCkATyKZeoF1+Gtw1+gdEP6pOCqj4=",
+			"4CQCKEyyOT/LysC17rsMXQ==",
+			"r9kcFt8+4Q6MnMjJHqJoSQ==",
+			"Hello, world!",
+			"IiQImHDLp7FUqR/b4sDybejMaLBUH6cXnZFlUrFlUg=="},
 	}
 
 	for _, tt := range referenceTests {
@@ -37,6 +53,10 @@ func TestSpecification(t *testing.T) {
 		keys := EncryptionKeys{
 			isTest: true,
 			salt:   b64(tt.salt),
+		}
+
+		if tt.auth != "" {
+			keys.SetPreSharedAuthSecret(b64(tt.auth))
 		}
 
 		keys.CreateEncryptionKeys(secret, encryptionContext)
